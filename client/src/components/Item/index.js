640d18f697dc73f8cc5de0e69e6341c184fe5ece
azeez1776 <aboud3am81@gmail.com>
2021-10-23 02:11:48 +0300

    added the Item component and made some setup

---
+++ client/src/components/Item/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Item() {
+    return (
+        <div className="item">
+            <div className="item_img">
+
+            </div>
+            <div className="item_description">
+                <div className="description"></div>
+                <div className="item_btn"></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Item
