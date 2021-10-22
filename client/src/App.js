603064c69cffed632272b2a4e5a07f285d3532a8
azeez1776 <aboud3am81@gmail.com>
2021-10-23 01:56:36 +0300

    added the react-router-dom package,imported the Explore page and made the Explore page the root page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+import { Route, Switch, BrowerRouter as Router } from 'react-router-dom'
 import './App.css';
+import Explore from './pages/Explore/index';
 
 function App() {
   return (
     <div className="App">
+      <Router
+        exact path='/'
+        name='explore'
+      >
+        <Explore />
+      </Router>
 
     </div>
   );
