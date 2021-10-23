bd5b76966ae5955381293496190c55bc581ff60f
azeez1776 <aboud3am81@gmail.com>
2021-10-23 19:57:48 +0300

    added the item component to the explore page and render multiple items mapped from items array

--- client/src/pages/Explore/index.js
+++ client/src/pages/Explore/index.js
@@ -1,11 +1,47 @@
 import React from 'react'
 import Item from '../../components/Item'
 
+
 function Explore() {
+
+    const items = [
+        {
+            title: 'salmon',
+            price: '2500tsh',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            img: 'Images/salmon.jpeg'
+        },
+        {
+            title: 'salmon',
+            price: '2500tsh',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            img: 'Images/salmon.jpeg'
+        },
+        {
+            title: 'salmon',
+            price: '2500tsh',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            img: 'Images/salmon.jpeg'
+        },
+        {
+            title: 'salmon',
+            price: '2500tsh',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            img: 'Images/salmon.jpeg'
+        },
+    ]
+
     return (
         <div className="explore">
+            {items.map((item, id) =>
+                <Item
+                    title={item.title}
+                    description={item.description}
+                    price={item.price}
+                    img={item.img}
+                />
+            )}
 
-            <Item />
         </div>
     )
 }
