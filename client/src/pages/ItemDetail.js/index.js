175baaaf10d5d8b36a1d4660804aa6b8a250350c
azeez1776 <aboud3am81@gmail.com>
2021-10-30 18:15:09 +0300

    added select tag for the user to select qty of item

--- client/src/pages/ItemDetail.js/index.js
+++ client/src/pages/ItemDetail.js/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
+
 function ItemDetail({ title, price, description, img }) {
+
+
+
     return (
         <div>
             <div className="item_detail_img">
@@ -10,7 +14,16 @@ function ItemDetail({ title, price, description, img }) {
                 <h2>{title}</h2>
                 <p>{description}</p>
             </div>
-            {/* Quantity */}
+            <div className="item_detail_qty">
+                <label for="cars">Qty: </label>
+                <select name="number of items" id="cars" t className="item_select">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
             {/* Button */}
         </div>
     )
