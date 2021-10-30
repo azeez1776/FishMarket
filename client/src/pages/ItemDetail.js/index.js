be28fb4d13832a5a2bf326d25a01cd4a0313b137
azeez1776 <aboud3am81@gmail.com>
2021-10-30 19:21:30 +0300

    made a flexbox layout

--- client/src/pages/ItemDetail.js/index.js
+++ client/src/pages/ItemDetail.js/index.js
@@ -6,26 +6,30 @@ function ItemDetail({ title, price, description, img }) {
 
 
     return (
-        <div>
-            <div className="item_detail_img">
-                <img src={img} alt={title} />
+        <div className="item_detail">
+            <div className="item_detail1">
+                <div className="item_detail_img">
+                    <img src={img} alt={title} />
+                </div>
             </div>
-            <div className="item_detail_info">
-                <h2>{title}</h2>
-                <p>{description}</p>
-            </div>
-            <div className="item_detail_qty">
-                <label for="cars">Qty: </label>
-                <select name="number of items" id="cars" t className="item_select">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-            </div>
-            <div className="item_detail_btn">
-                <button className="item_btn_1">ADD TO CART</button>
+            <div className="item_detail2">
+                <div className="item_detail_info">
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                </div>
+                <div className="item_detail_qty">
+                    <label for="cars">Qty: </label>
+                    <select name="number of items" id="cars" t className="item_select">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <div className="item_detail_btn">
+                    <button className="item_btn_1">ADD TO CART</button>
+                </div>
             </div>
         </div>
     )
