85b2ed3ada7a495559869d8bb1edda64149acb9c
azeez1776 <aboud3am81@gmail.com>
2021-10-30 14:07:09 +0300

    made the ItemDetail folder and added a js file and added item description

---
+++ client/src/pages/ItemDetail.js/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ItemDetail() {
+    return (
+        <div>
+            {/* image */}
+            <div className="item_detail_info">
+                <h2>Samaki</h2>
+                <p>Consequat eu minim ea ullamco qui enim veniam fugiat non magna dolore officia aute. Laboris voluptate irure pariatur labore culpa fugiat adipisicing velit magna amet nisi reprehenderit qui amet. Fugiat sunt ullamco ea velit sit veniam quis commodo proident veniam. Labore sint excepteur sint ut veniam culpa mollit ad aliqua nisi.</p>
+            </div>
+            {/* Quantity */}
+            {/* Button */}
+        </div>
+    )
+}
+
+export default ItemDetail
