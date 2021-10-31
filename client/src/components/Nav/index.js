d0484f47ada64b702d5391994646fa84c191d025
azeez1776 <aboud3am81@gmail.com>
2021-10-31 14:40:30 +0300

    styled the nav bar together with the basket

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,9 +6,15 @@ function Nav() {
     return (
         <div className="nav">
             <div className="nav_header">
-                <img className="nav_img" src="Images/fishbone.png" alt="fishbone" />
-                <h1 className="nav_title">FISH MARKET</h1>
-                <img className="nav_basket" src="Image/basket.png" alt="basket" />
+                <div className="nav_head">
+                    <div className="title">
+                        <img className="nav_img" src="Images/fishbone.png" alt="fishbone" />
+                        <h1 className="nav_title">FISH MARKET</h1>
+                    </div>
+                    <div className="basket">
+                        <img className="nav_basket" src="Images/basket.png" alt="basket" />
+                    </div>
+                </div>
             </div>
             <div className="nav_links">
 
