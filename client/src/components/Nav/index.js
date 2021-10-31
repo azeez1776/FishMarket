235df6aefb5cd0e10cb9e72d22dded85d541d193
azeez1776 <aboud3am81@gmail.com>
2021-10-31 13:55:56 +0300

    included the basket image in the nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,6 +8,7 @@ function Nav() {
             <div className="nav_header">
                 <img className="nav_img" src="Images/fishbone.png" alt="fishbone" />
                 <h1 className="nav_title">FISH MARKET</h1>
+                <img className="nav_basket" src="Image/basket.png" alt="basket" />
             </div>
             <div className="nav_links">
 
