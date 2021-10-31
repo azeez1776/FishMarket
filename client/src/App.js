07ebb19cde77573cf30809442df6a18a8f96df2b
azeez1776 <aboud3am81@gmail.com>
2021-10-31 13:13:57 +0300

    imported Provider and included it

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,23 @@ import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 import Explore from './pages/Explore/index';
 import store from './app/store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route
-            exact path='/'
-            name='explore'
-          >
-            <Explore />
-          </Route>
-        </Switch>
-      </Router>
-
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route
+              exact path='/'
+              name='explore'
+            >
+              <Explore />
+            </Route>
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
