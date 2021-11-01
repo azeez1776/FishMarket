b587b58c177b602dfe4d38f819a9eca758b5d70c
azeez1776 <aboud3am81@gmail.com>
2021-11-02 00:01:26 +0300

    added truthy reducer to the store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
+import modalReducer from '../features/modalTruthy/modalTruthy'
 
 export default configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        truthy: modalReducer
     },
 })
