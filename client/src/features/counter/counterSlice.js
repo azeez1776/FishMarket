8edd1677226faab600319f0145fdca94f3a8fa94
azeez1776 <aboud3am81@gmail.com>
2021-11-01 23:42:08 +0300

    updated the createSlice, added showModal and truthy

--- client/src/features/counter/counterSlice.js
+++ client/src/features/counter/counterSlice.js
@@ -3,15 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
     name: 'counter',
     initialState: {
-        value: 0
+        value: 0,
+        truthy: false
     },
     reducers: {
         increment: (state) => {
             state.value += 1
+        },
+        showModal: (state) => {
+            state.truthy = !state.truthy
         }
 
     }
-})
+}
+)
 
-export const { increment } = counterSlice.actions;
+export const { increment, showModal } = counterSlice.actions;
 export default counterSlice.reducer;
