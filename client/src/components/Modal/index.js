8076cb6736e9c2195d536308d1f89126b7193dcd
azeez1776 <aboud3am81@gmail.com>
2021-11-02 02:44:08 +0300

    designed the initial state of the modal

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -4,7 +4,15 @@ import '../../assets/CSS/styles.css'
 function Modal() {
     return (
         <div className="modal">
-            <h1>Esse quis culpa incididunt labore nulla voluptate cupidatat. Velit cillum laboris sunt aliquip nostrud adipisicing velit magna ad laborum velit elit. Dolore laboris ipsum esse cupidatat voluptate ipsum officia ea deserunt ullamco fugiat anim consectetur non.</h1>
+
+            <div className="modal_info">
+                <div className="modal_header">
+                    <h1 className="modal_title">Basket</h1>
+                </div>
+                <div className="modal_description">
+                    <h1>Esse quis culpa incididunt labore nulla voluptate cupidatat. Velit cillum laboris sunt aliquip nostrud adipisicing velit magna ad laborum velit elit. Dolore laboris ipsum esse cupidatat voluptate ipsum officia ea deserunt ullamco fugiat anim consectetur non.</h1>
+                </div>
+            </div>
         </div>
     )
 }
