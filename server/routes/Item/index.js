860b526721b8ff2610540c4d79148f40c7dd81b3
azeez1776 <aboud3am81@gmail.com>
2021-11-03 17:37:48 +0300

    added the item router, imported the item model and implemented the get functionality

--- server/routes/Item/index.js
+++ server/routes/Item/index.js
@@ -1,3 +1,16 @@
 const express = require('express');
-const Item = require('../../model/Item/')
+const Items = require('../../model/Item/')
+
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const item = await Items.find()
+        res.json(item)
+    }
+    catch (error) {
+        res.send('error in getting items')
+    }
+})
 
